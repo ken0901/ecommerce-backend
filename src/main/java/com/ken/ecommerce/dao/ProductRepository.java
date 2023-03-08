b0d4f8bb3af27a08acc3fb13f8e5e324c3840289
ken0901 <ken5260715@gmail.com>
2023-03-07 22:20:50 -0800

    Allow crossOrigin for frontend

--- src/main/java/com/ken/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ken/ecommerce/dao/ProductRepository.java
@@ -3,7 +3,8 @@ package com.ken.ecommerce.dao;
 import com.ken.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
